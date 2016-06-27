c49e8a8a7c684f7273b1bf83b380f14d0f916ff9
Jan von Pichowski <janvonpichowski@gmail.com>
2016-06-27 20:56:09 +0200

    Added example files

---
+++ src/main/java/com/example/deploy/ExampleLibrary.java
@@ -0,0 +1,11 @@
+package com.example.deploy;
+
+/**
+ * Created by jan on 27.06.16.
+ */
+public class ExampleLibrary {
+
+    public static String getMessage(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/deploy/ExampleLibraryTest.java
@@ -0,0 +1,12 @@
+package com.example.deploy;
+
+/**
+ * Created by jan on 27.06.16.
+ */
+public class ExampleLibraryTest {
+
+    public static void main(String... args){
+        System.out.println(ExampleLibrary.getMessage());
+    }
+
+}
